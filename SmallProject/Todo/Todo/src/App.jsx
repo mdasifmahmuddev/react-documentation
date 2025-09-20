ef4a24d2b0d6afd6edaaebe5ea3baf9b08f23f7b
mdasifmahmuddev <md.asif.mahmud.dev@gmail.com>
2025-09-21 03:11:33 +0600

    start to making todo app for basic understand properly

---
+++ SmallProject/Todo/Todo/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <h1> My Todo App  </h1>
+    </>
+  )
+}
+
+export default App
